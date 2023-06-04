1cec879fab0a4e1f13d1e0d581e3c86572389b93
Bastien <neitsabast@gmail.com>
2023-06-04 23:04:38 +0200

    script shi-fou-mi

---
+++ README.md
@@ -0,0 +1,3 @@
+## Bash Learning
+
+Bash Learning est un repository dans lequel se trouve divers scripts en Bash que j'ai réalisé afin d'apprendre Bash.
---
+++ shi-fou-mi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Simple jeu Pierre-Feuille-Ciseaux en 1 manche
+
+# Les actions possibles
+game_action=("Pierre" "Feuille" "Ciseaux")
+
+echo "Vous allez jouer une partie de Pierre-Feuille-Ciseaux en 1 manche"
+echo "1. Pierre"
+echo "2. Feuille"
+echo "3. Ciseaux"
+read -p "Veuillez choisir votre action en tapant le nombre correspondant a votre action, puis [ENTER] : " userInput #Recupère l'input de l'utilisateur
+
+# Check l'input de l'utilisateur
+if [ $userInput -lt 1 -o $userInput -gt 3 ]; then 
+    echo "Votre action '$userInput' est introuvable."
+    exit 0;
+fi
+
+# Génère une valeur random pour l'adversaire
+botValue=$(( $RANDOM % 3 ))
+
+# Reduit de 1 la valeur de l'user
+userInput=$((userInput - 1))
+
+echo ""
+echo "Votre action : ${game_action[$userInput]}"
+echo "Action de l'adversaire : ${game_action[$botValue]}"
+echo ""
+
+if [[ $userInput -eq $botValue ]]; then
+    echo "Égalité !"
+elif [[ ($userInput -gt $botValue) || ($userInput -eq 2 && $botValue -eq 0) ]]; then
+    echo "Défaite !"
+else
+    echo "Victoire !"
+fi
+
+exit 0
